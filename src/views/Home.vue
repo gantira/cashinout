d38101d4aaee7ca0a131f2feb7d0c6d322f17e6c
Angga Gantira <angga.gantira@gmail.com>
2021-01-12 16:03:28 +0700

    7 Setup untuk frontendnya

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mx-auto">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel repudiandae
+    minima voluptatibus. Veniam eos exercitationem deserunt obcaecati rerum
+    dolor quis nam, excepturi vel itaque a deleniti quas sit. Totam, quas?
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
+
+<style>
+</style>
